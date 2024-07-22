e05c91a851e7fb60098b9cd3a5eace76d3d31aac
luxasjer <jiap@hotmail.com>
2024-07-22 22:28:05 +0800

    feat: test lastUpdated

--- assets/posts_index.md.CE4fqF7P.lean.js
+++
@@ -1,1 +0,0 @@
-import{_ as t,c as e,o as a,a3 as o}from"./chunks/framework.D42TvibZ.js";const u=JSON.parse('{"title":"文章列表","description":"","frontmatter":{},"headers":[],"relativePath":"posts/index.md","filePath":"posts/index.md"}'),s={name:"posts/index.md"},c=o("",3),i=[c];function n(_,r,d,p,l,h){return a(),e("div",null,i)}const f=t(s,[["render",n]]);export{u as __pageData,f as default};
